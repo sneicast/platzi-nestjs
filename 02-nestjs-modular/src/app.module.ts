d4d3f5a210f25b04d98d80d9e0175e5b43248eca
Sneider Castillo <sneicast@hotmail.com>
2022-08-04 01:36:54 -0500

    Tipado en config

--- 02-nestjs-modular/src/app.module.ts
+++ 02-nestjs-modular/src/app.module.ts
@@ -8,11 +8,12 @@ import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { enviroments } from './enviroments';
-
+import config from './config';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
       isGlobal: true,
     }),
     HttpModule,
--- 02-nestjs-modular/src/app.service.ts
+++ 02-nestjs-modular/src/app.service.ts
@@ -1,17 +1,21 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
     // @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
-    private config: ConfigService,
+    // private config: ConfigService,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
   getHello(): string {
     //console.log(this.tasks);
-    const apiKey = this.config.get('API_KEY');
-    const dbName = this.config.get('DATABASE_NAME');
+    // const apiKey = this.config.get<string>('API_KEY');
+    // const dbName = this.config.get('DATABASE_NAME');
+    const apiKey = this.configService.apiKey;
+    const dbName = this.configService.database.name;
     return `Hello World! ${apiKey} ${dbName}`;
   }
 }
