ac772dcd54d12d8f442b34471236f944331ce6ea
Sneider Castillo <sneicast@hotmail.com>
2022-08-03 23:12:31 -0500

    Base del proyecto nestjs modular

---
+++ 02-nestjs-modular/src/products/controllers/products/products.controller.ts
@@ -0,0 +1,68 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { ParseIntPipe } from 'src/common/parse-int.pipe';
+import {
+  CreateProductDto,
+  UpdateProductDto,
+} from 'src/products/dtos/products.dtos';
+import { ProductsService } from 'src/products/services/products/products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  @Get()
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    // return {
+    //   message: `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`,
+    // };
+    return this.productsService.findAll();
+  }
+
+  @Get('filter')
+  getProductFilter() {
+    return `yo soy un filter`;
+  }
+
+  @Get(':productId')
+  @HttpCode(HttpStatus.ACCEPTED)
+  getOne(@Param('productId', ParseIntPipe) productId: number) {
+    // response.status(200).send({
+    //   message: `product ${productId}`,
+    // });
+    return this.productsService.findOne(productId);
+  }
+
+  @Post()
+  create(@Body() payload: CreateProductDto) {
+    // return {
+    //   message: 'accion de crear',
+    //   payload,
+    // };
+    return this.productsService.create(payload);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() payload: UpdateProductDto) {
+    return this.productsService.update(+id, payload);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.productsService.remove(+id);
+  }
+}
---
+++ 02-nestjs-modular/src/products/products.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ProductsController } from './controllers/products/products.controller';
+import { ProductsService } from './services/products/products.service';
+import { CategoriesController } from './controllers/categories/categories.controller';
+
+@Module({
+  controllers: [ProductsController, CategoriesController],
+  providers: [ProductsService]
+})
+export class ProductsModule {}
